c113e3c9143d37fd4c02d7bf496f9d9e39d01c02
Andy c <81699558+andycr123@users.noreply.github.com>
2023-05-25 16:51:47 -0500

    changes

---
+++ crud-api-morty/src/helpers/getCharacter.js
@@ -0,0 +1,31 @@
+import axios from 'axios'
+import Swal from 'sweetalert2'
+
+export const getCharacter = async (filter) => {
+    const result = await axios.get(`https://rickandmortyapi.com/api/character${filter}`).then((res) => {
+        if (res.status === 200) {
+            const listPersonajesRestult = res.data.results.map(({ image, id, gender, name, species, status }) => ({
+                image,
+                id,
+                gender,
+                name,
+                species,
+                status
+            }))
+            const result = {
+                countPages: res.data.info.pages,
+                ListCharacters:listPersonajesRestult
+            }
+            return result
+        }
+    }).catch((err) => {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: err.message
+        })
+        console.error(err)
+    })
+
+    return result
+}
---
+++ crud-api-morty/src/helpers/getListaOpiniones.js
@@ -0,0 +1,20 @@
+import Swal from 'sweetalert2'
+import { firebase } from '../firebase'
+export const getListaOpiniones = async () => {
+
+    try {
+        const db = firebase.firestore()
+        const data = await db.collection('OpinionesPersonajes').get()
+        const listPersonajesRestult = data.docs.map(doc => ({
+            id: doc.id,
+            ...doc.data()
+        }))
+        return listPersonajesRestult
+    } catch (error) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Ocurrio un error al consultar a la base de datos.'
+        })
+    }
+}
